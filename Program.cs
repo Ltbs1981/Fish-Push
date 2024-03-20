340afd5ccbe313cbeb2339979af102b4435fa947
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-20 11:44:48 -0300

    início de teste

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("Fish push");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fish    ");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Push");
+    }
+    e
+    }
+}
